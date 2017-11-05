8fb39709a2a988f5d850706ebc10a09fd7b8e72f
Xiao Zeng <zengxiao1028@gmail.com>
2017-11-04 21:30:32 -0400

    test

---
+++ dataset/VOCdevkit/split_train_val.py
@@ -0,0 +1,40 @@
+import os
+from shutil import copyfile
+
+
+def gen_voc(VOC_Dev_Folder, des_folder):
+
+    split_file = os.path.join(VOC_Dev_Folder, 'ImageSets/Main/train.txt')
+    if os.path.exists(split_file):
+        with open(split_file,'r') as f:
+            lines = [line.rstrip() for line in f]
+            print(lines)
+
+
+    for file in lines:
+        annotation_file = os.path.join(os.path.join(VOC_Dev_Folder,'Annotations'), file + '.xml' )
+        copyfile(annotation_file, os.path.join( os.path.join(des_folder,'train_ann'), file + '.xml'))
+
+        annotation_file = os.path.join(os.path.join(VOC_Dev_Folder, 'JPEGImages'), file + '.jpg')
+        copyfile(annotation_file, os.path.join(os.path.join(des_folder, 'train'), file + '.jpg'))
+
+    split_file = os.path.join(VOC_Dev_Folder, 'ImageSets/Main/val.txt')
+    if os.path.exists(split_file):
+        with open(split_file, 'r') as f:
+            lines = [line.rstrip() for line in f]
+            print(lines)
+
+    for file in lines:
+        annotation_file = os.path.join(os.path.join(VOC_Dev_Folder, 'Annotations'), file + '.xml')
+        copyfile(annotation_file, os.path.join(os.path.join(des_folder, 'val_ann'), file + '.xml'))
+
+        annotation_file = os.path.join(os.path.join(VOC_Dev_Folder, 'JPEGImages'), file + '.jpg')
+        copyfile(annotation_file, os.path.join(os.path.join(des_folder, 'val'), file + '.jpg'))
+
+
+
+if __name__ == '__main__':
+    gen_voc('/Users/xiaozeng/PycharmProjects/YoloV2/dataset/VOCdevkit/VOC2007',
+            '/Users/xiaozeng/PycharmProjects/YoloV2/dataset/voc2007')
+
+
