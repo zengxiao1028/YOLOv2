e890e01c519ad11b878cbb1e12d7924631050399
Xiao Zeng <zengxiao1028@gmail.com>
2017-11-12 17:13:46 -0500

    test

--- xiaofrontend.py
+++ xiaofrontend.py
@@ -3,7 +3,7 @@ from keras.layers import Reshape, Activation, Conv2D, Input, MaxPooling2D, Batch
 import os
 import tensorflow as tf
 import numpy as np
-import cv2
+from keras.layers.merge import concatenate
 from keras.applications.mobilenet import MobileNet
 import shutil
 from keras.layers.advanced_activations import LeakyReLU
@@ -46,34 +46,17 @@ class XiaoYOLO(YOLO):
         input_image = Input(shape=(self.input_size, self.input_size, 3))
         self.true_boxes = Input(shape=(1, 1, 1, max_box_per_image, 4))
 
-        if architecture == 'Inception3':
-            self.feature_extractor = Inception3Feature(self.input_size)
-        elif architecture == 'SqueezeNet':
-            self.feature_extractor = SqueezeNetFeature(self.input_size)
-        elif architecture == 'MobileNet':
-            self.feature_extractor = MobileNetFeature(self.input_size)
-        elif architecture == 'Full Yolo':
-            self.feature_extractor = FullYoloFeature(self.input_size, kernel_regularizer=kr)
-        elif architecture == 'Tiny Yolo':
-            self.feature_extractor = TinyYoloFeature(self.input_size)
-        else:
-            raise Exception(
-                'Architecture not supported! Only support Full Yolo, Tiny Yolo, MobileNet, SqueezeNet, and Inception3 at the moment!')
+        features = self._make_body(input_image,kr)
 
-        print(self.feature_extractor.get_output_shape())
-        self.grid_h, self.grid_w = self.feature_extractor.get_output_shape()
+        print(features.get_output_shape())
 
-        #which level of feature to use
-        features = self.feature_extractor.feature_extractor_model.get_layer('out_22').output
-        output = self._make_head(features,kr)
+        self.grid_h, self.grid_w = features.get_output_shape()
 
-        #low_features = self.feature_extractor.feature_extractor_model.get_layer('out_2').output
-        #output = self._make_low_head(low_features,kr)
-
+        output = self._make_head(features, kr)
 
         output = Lambda(lambda args: args[0])([output, self.true_boxes])
 
-        self.model = Model([self.feature_extractor.feature_extractor_model.input, self.true_boxes], output)
+        self.model = Model([input_image, self.true_boxes], output)
 
         # initialize the weights of the detection layer
         layer = self.model.layers[-4]
@@ -85,7 +68,152 @@ class XiaoYOLO(YOLO):
         # print a summary of the whole model
         self.model.summary()
 
-    def _make_low_head(self, features, kr):
+    def _make_body(self, input_image, kernel_regularizer):
+        # Layer 1
+        x = Conv2D(32, (3, 3), strides=(1, 1), padding='same', name='conv_1', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(input_image)
+        x = BatchNormalization(name='norm_1')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 2), name='out_1')(x)
+
+        # Layer 2
+        x = Conv2D(64, (3, 3), strides=(1, 1), padding='same', name='conv_2', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_2')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 2), name='out_2')(x)
+
+        # Layer 3
+        x = Conv2D(128, (3, 3), strides=(1, 1), padding='same', name='conv_3', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_3')(x)
+        x = LeakyReLU(alpha=0.1, name='out_3')(x)
+
+        # Layer 4
+        x = Conv2D(64, (1, 1), strides=(1, 1), padding='same', name='conv_4', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_4')(x)
+        x = LeakyReLU(alpha=0.1, name='out_4')(x)
+
+        # Layer 5
+        x = Conv2D(128, (3, 3), strides=(1, 1), padding='same', name='conv_5', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_5')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 2), name='out_5')(x)
+
+        # Layer 6
+        x = Conv2D(256, (3, 3), strides=(1, 1), padding='same', name='conv_6', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_6')(x)
+        x = LeakyReLU(alpha=0.1, name='out_6')(x)
+
+        # Layer 7
+        x = Conv2D(128, (1, 1), strides=(1, 1), padding='same', name='conv_7', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_7')(x)
+        x = LeakyReLU(alpha=0.1, name='out_7')(x)
+
+        # Layer 8
+        x = Conv2D(256, (3, 3), strides=(1, 1), padding='same', name='conv_8', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_8')(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = MaxPooling2D(pool_size=(2, 2), name='out_8')(x)
+
+        # Layer 9
+        x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', name='conv_9', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_9')(x)
+        x = LeakyReLU(alpha=0.1, name='out_9')(x)
+
+        # Layer 10
+        x = Conv2D(256, (1, 1), strides=(1, 1), padding='same', name='conv_10', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_10')(x)
+        x = LeakyReLU(alpha=0.1, name='out_10')(x)
+
+        # Layer 11
+        x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', name='conv_11', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_11')(x)
+        x = LeakyReLU(alpha=0.1, name='out_11')(x)
+
+        # Layer 12
+        x = Conv2D(256, (1, 1), strides=(1, 1), padding='same', name='conv_12', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_12')(x)
+        x = LeakyReLU(alpha=0.1, name='out_12')(x)
+
+        # Layer 13
+        x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', name='conv_13', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_13')(x)
+        x = LeakyReLU(alpha=0.1, name='out_13')(x)
+
+        skip_connection = x
+
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+
+        # Layer 14
+        x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_14', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_14')(x)
+        x = LeakyReLU(alpha=0.1, name='out_14')(x)
+
+        # Layer 15
+        x = Conv2D(512, (1, 1), strides=(1, 1), padding='same', name='conv_15', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_15')(x)
+        x = LeakyReLU(alpha=0.1, name='out_15')(x)
+
+        # Layer 16
+        x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_16', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_16')(x)
+        x = LeakyReLU(alpha=0.1, name='out_16')(x)
+
+        # Layer 17
+        x = Conv2D(512, (1, 1), strides=(1, 1), padding='same', name='conv_17', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_17')(x)
+        x = LeakyReLU(alpha=0.1, name='out_17')(x)
+
+        # Layer 18
+        x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_18', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_18')(x)
+        x = LeakyReLU(alpha=0.1, name='out_18')(x)
+
+        # Layer 19
+        x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_19', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_19')(x)
+        x = LeakyReLU(alpha=0.1, name='out_19')(x)
+
+        # Layer 20
+        x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_20', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_20')(x)
+        x = LeakyReLU(alpha=0.1, name='out_20')(x)
+
+        # Layer 21
+        skip_connection = Conv2D(64, (1, 1), strides=(1, 1), padding='same', name='conv_21', use_bias=False,
+                                 kernel_regularizer=kernel_regularizer)(skip_connection)
+        skip_connection = BatchNormalization(name='norm_21')(skip_connection)
+        skip_connection = LeakyReLU(alpha=0.1)(skip_connection)
+        skip_connection = Lambda(lambda y: tf.space_to_depth(y, block_size=2))(skip_connection)
+
+        x = concatenate([skip_connection, x])
+
+        # Layer 22
+        x = Conv2D(1024, (3, 3), strides=(1, 1), padding='same', name='conv_22', use_bias=False,
+                   kernel_regularizer=kernel_regularizer)(x)
+        x = BatchNormalization(name='norm_22')(x)
+        x = LeakyReLU(alpha=0.1, name='out_22')(x)
+        return x
+
+    def _make_sub_head(self, features, kr):
 
         # sub_conv1
         sub_x = Conv2D(32,
